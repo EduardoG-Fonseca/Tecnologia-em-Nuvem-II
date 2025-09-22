88c3853727d984d268d4b2aa0e698f2adf1df972
EduardoG-Fonseca <163783426+EduardoG-Fonseca@users.noreply.github.com>
2025-09-22 07:39:01 -0300

    Update Converter_binario.php

--- App3/Converter_binario.php
+++ App3/Converter_binario.php
@@ -1,1 +1,19 @@
+<?php
 
+function stringParaBinario ($string){
+    $binario = '';
+    foreach(str_split($string) as $char){
+        $binario .= str_pad(decbin(ord($char)), 8, '0', STR_PAD_LEFT) . ' ';
+    }
+    return $binario;
+}
+
+//exemplo de uso
+$frase = "Eduardo Gabriel da Fonseca";
+$fraseBinario = stringParaBinario($frase);
+
+//exibe a frase original e a frase em binário
+echo "Frase original: " . $frase . "<br>";
+echo "Frase em binário: " . $fraseBinario;
+
+?>
